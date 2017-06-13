2f0ac7d193a4ca0e4887f48f2be3c05c8764a15c
Shafeen Rahman <shafeen@shafeenr.me>
2017-06-13 08:40:26 -0400

    Update sketch_jan10a.ino

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -1,63 +1,85 @@
-#include <Servo.h> // Preprocessor scans Servo header file
+#include <Servo.h>
 
-/* 
+#define oneDeg 6.5
+#define oneCM 60
 
-Here we figured out approximately how much milliseconds in delay 
-would let our bot move approximately one degree and one centimetre
+Servo leftMotor;
+Servo rightMotor;
 
-The #define directive is used instead of
-declaring variable of data type int with const
-keyword because the former uses less memory
-
-*/
-
-#define oneDeg 6.5 // 1 degree is around 6.5 ms
-#define oneCM 60 // 1 centimetre is around 60 ms
-
-Servo leftMotor; // Declare left motor of Servo data type
-Servo rightMotor; // Declare right motor of Servo data type
+int buttonPin = 4;
+byte button = 0;
 
 void setup() {
+  leftMotor.attach(12);
+  rightMotor.attach(13);
 
-  leftMotor.attach(13); // Broadcast left motor's signal to pin 13
-  rightMotor.attach(12); // Broadcast right motor's signal to pin 12
+  delay(50);
+  pinMode(10, INPUT);
+  delay(50);
+  pinMode(11, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  /*
-  
-  Due to placement differences, for example to go straight, the right motor moves clockwise while
-  the left motor moves counterclockwise.
-  
-  1300 microseconds - Full speed clockwise
-  1400 microseconds - Linear speed zone starts
-  1500 microseconds - No motion
-  1600 microseconds - Linear speed zone ends
-  1700 microseconds - Full speed counter-clockwise
-  
-  */
-
-  // Travel 165 centimetres straight forward
-  
-  leftMotor.writeMicroseconds(1300); // Left wheel full speed counter-clockwise
-  rightMotor.writeMicroseconds(1700); // Right wheel full speed clockwise
-  delay(oneCM * 165);
-  
-  // Turn left in place
-  
-  leftMotor.writeMicroseconds(1300);  // Left wheel full speed counter-clockwise
-  rightMotor.writeMicroseconds(1300); // Right wheel full speed counter-clockwise
-  delay(oneDeg * 91); // Set for 91 degrees as a little tweak to turn harder
-  
-  // Travel 54 centimetres straight forward
-  
-  leftMotor.writeMicroseconds(1300);  // Left wheel full speed clockwise
-  rightMotor.writeMicroseconds(1700); // Right wheel full speed counter-clockwise
-  delay(oneCM * 54);
-
-  leftMotor.detach(); // Stop broadcast of signals to left motor
-  rightMotor.detach(); // Stop broadcast of signals to right motor
 }
 
 void loop() {
+
+  if (digitalRead(buttonPin) == HIGH) {
+    lineTracker();
+  }
+  else if (digitalRead(buttonPin) == LOW && button == 0) {
+    leftMotor.writeMicroseconds(1500);
+    rightMotor.writeMicroseconds(1700);
+    delay(oneDeg * 90);
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1700);
+    delay(1500);
+    leftMotor.writeMicroseconds(1500);
+    rightMotor.writeMicroseconds(1500);
+    delay(1500);
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1700);
+    delay(1500);
+    button = 1;
+  }
+  else if (digitalRead(buttonPin) == HIGH && button == 1) {
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1300);
+    delay(oneDeg * 110);
+    
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1700);
+    delay(oneCM * 25);
+    
+    leftMotor.writeMicroseconds(1250);
+    rightMotor.writeMicroseconds(1540);
+    delay(oneCM * 90);
+  }
 }
 
+void lineTracker() {
 
+  int leftSensor = digitalRead(11);
+  int rightSensor = digitalRead(10);
+
+  if ((leftSensor == HIGH) && (rightSensor == HIGH)) {
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1700);
+  }
+  else if ((leftSensor == LOW) && (rightSensor == HIGH)) {
+    leftMotor.writeMicroseconds(1500);
+    rightMotor.writeMicroseconds(1700);
+  }
+  else if ((leftSensor == HIGH) && (rightSensor == LOW)) {
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1500);
+  }
+  else if ((leftSensor == LOW) && (rightSensor == LOW)) {
+    leftMotor.writeMicroseconds(1300);
+    rightMotor.writeMicroseconds(1700);
+  }
+  else {
+    leftMotor.writeMicroseconds(1500);
+    rightMotor.writeMicroseconds(1500);
+  }
+
+}
